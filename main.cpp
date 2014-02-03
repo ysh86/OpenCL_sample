fc273742b0b07c9b84978afcc22f8018f60a08ab
Yoshiharu <ysh86@users.noreply.github.com>
2014-02-03 14:03:51 +0900

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,172 @@
+#define __CL_ENABLE_EXCEPTIONS
+
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <OpenCL/cl.hpp>
+#else
+#include <CL/cl.hpp>
+#endif
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+const size_t ch = 4;
+const size_t width = 1024;
+const size_t height = 1024 * 2;
+
+const cl::NDRange kernelRangeGlobal(width, height);
+const cl::NDRange kernelRangeLocal(32,8);
+
+#define kernelName "hello"
+const char * kernelStr  = ""
+"__kernel void " kernelName "("
+"    __global const unsigned int *a,"
+"    __global const unsigned int *b,"
+"    __global unsigned int *c"
+")"
+"{"
+"    size_t gid = get_global_id(1) * 1024 + get_global_id(0);"
+"    /*size_t gid = mad24(get_global_id(1), (size_t)1024, get_global_id(0));*/"
+""
+"    uint temp_a = a[gid];"
+"    uint temp_b = b[gid];"
+"    uint temp_c;"
+"    temp_c  = ((temp_a & 0x000000FF) + (temp_b & 0x000000FF)) & 0x000000FF;"
+"    temp_c |= ((temp_a & 0x0000FF00) + (temp_b & 0x0000FF00)) & 0x0000FF00;"
+"    temp_c |= ((temp_a & 0x00FF0000) + (temp_b & 0x00FF0000)) & 0x00FF0000;"
+"    temp_c |= ((temp_a & 0xFF000000) + (temp_b & 0xFF000000)) & 0xFF000000;"
+""
+"    c[gid] = temp_c;"
+"}";
+
+int
+main(void)
+{
+    cl_int err = CL_SUCCESS;
+    try {
+        // Platform
+        // --------------------------------------------
+        std::cout << "Platform" << std::endl;
+        std::cout << "=======================================" << std::endl;
+        std::vector<cl::Platform> platforms;
+        err |= cl::Platform::get(&platforms);
+        int n = platforms.size();
+        for (int i = 0; i < n; i++) {
+            std::cout << i << std::endl;
+            std::cout << "---------------------------------------" << std::endl;
+
+            static const int names[] = {
+                CL_PLATFORM_PROFILE,
+                CL_PLATFORM_VERSION,
+                CL_PLATFORM_NAME,
+                CL_PLATFORM_VENDOR,
+                CL_PLATFORM_EXTENSIONS,
+            };
+            int n_names = sizeof(names) / sizeof(names[0]);
+            for (int j = 0; j < n_names; j++) {
+                std::string param;
+                err |= platforms[i].getInfo(names[j], &param);
+                std::cout << param << std::endl;
+            }
+        }
+        if (n == 0) {
+            std::cout << "Platform size 0\n";
+            return -1;
+        }
+
+        // Context
+        // --------------------------------------------
+        cl_context_properties properties[] = {
+            CL_CONTEXT_PLATFORM,
+            (cl_context_properties)(platforms[0])(),
+            0
+        };
+        cl::Context context(CL_DEVICE_TYPE_GPU, properties); 
+
+        // Device
+        // --------------------------------------------
+        std::cout << "Device" << std::endl;
+        std::cout << "=======================================" << std::endl;
+        std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+        int n_dev = devices.size();
+        for (int i = 0; i < n_dev; i++) {
+            std::cout << i << std::endl;
+            std::cout << "---------------------------------------" << std::endl;
+
+            static const int names[] = {
+                CL_DEVICE_NAME,
+                CL_DEVICE_VENDOR,
+                CL_DEVICE_PROFILE,
+                CL_DEVICE_VERSION,
+                CL_DRIVER_VERSION,
+                CL_DEVICE_OPENCL_C_VERSION,
+                CL_DEVICE_EXTENSIONS,
+            };
+            int n_names = sizeof(names) / sizeof(names[0]);
+            for (int j = 0; j < n_names; j++) {
+                std::string param;
+                err |= devices[i].getInfo(names[j], &param);
+                std::cout << param << std::endl;
+            }
+        }
+
+        // Build
+        // --------------------------------------------
+        cl::Program::Sources sources(
+            1,
+            std::make_pair(kernelStr, strlen(kernelStr))
+        );
+        cl::Program program_ = cl::Program(context, sources);
+        err |= program_.build(devices);
+        cl::Kernel kernel(program_, kernelName, &err);
+
+        // Execution
+        // --------------------------------------------
+        cl::Buffer a(context, CL_MEM_READ_ONLY, width*ch*height);
+        cl::Buffer b(context, CL_MEM_READ_ONLY, width*ch*height);
+        cl::Buffer c(context, CL_MEM_WRITE_ONLY, width*ch*height);
+        err |= kernel.setArg(0, a);
+        err |= kernel.setArg(1, b);
+        err |= kernel.setArg(2, c);
+
+        cl::CommandQueue queue(
+            context,
+            devices[0],
+            CL_QUEUE_PROFILING_ENABLE, //0,
+            &err);
+
+        cl::Event event;
+        err |= queue.enqueueNDRangeKernel(
+            kernel,
+            cl::NullRange,
+            kernelRangeGlobal,
+            kernelRangeLocal,
+            NULL,
+            &event);
+        err |= event.wait();
+
+        // Profiling
+        // --------------------------------------------
+        cl_ulong start;
+        cl_ulong end;
+        err |= event.getProfilingInfo(CL_PROFILING_COMMAND_START, &start);
+        err |= event.getProfilingInfo(CL_PROFILING_COMMAND_END, &end);
+        std::cout
+        << "Kernel "
+        << kernelName
+        << "(): "
+        << static_cast<double>(end - start) / 1000.0 / 1000.0
+        << " [msec]"
+        << std::endl;
+    }
+    catch (cl::Error err) {
+        std::cerr 
+        << "ERROR: "
+        << err.what()
+        << "("
+        << err.err()
+        << ")"
+        << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
